e65239d432137e93454f70c2bf0c310c6296e605
Thinup_manager <99749279+thinup@users.noreply.github.com>
2022-04-30 02:27:04 +0900

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,24 +1,30 @@
-"""beginnersite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from re import search
+from django.urls import path
+from app import views
+from app import search
+from app import forms
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('game/', include('game.urls')),
-    path('thinup/', include('thinup.urls')),
+    path('', views.IndexView.as_view(), name='index'),
+    path('sns/twitter/', views.TwitterView.as_view(), name='twitter'),
+    path('sns/instagram/', views.InstagramView.as_view(), name='instagram'),
+    path('sns/youtube/', views.YoutubeView.as_view(), name='youtube'),
+    path('pc/pc-index/', views.PcIndexView.as_view(), name='pc_index'),
+    path('pc/pc-1/', views.Pc1View.as_view(), name='pc_1'),
+    path('pc/pc-2/', views.Pc2View.as_view(), name='pc_2'),
+    path('pc/pc-3/', views.Pc3View.as_view(), name='pc_3'),
+    path('python/python-index/', views.PythonIndexView.as_view(), name='python_index'),
+    path('python/python-1/', views.Python1View.as_view(), name='python_1'),
+    path('python/python-2/', views.Python2View.as_view(), name='python_2'),
+    path('python/python-3/', views.Python3View.as_view(), name='python_3'),
+    path('python/python-4/', views.Python4View.as_view(), name='python_4'),
+    path('python/python-5/', views.Python5View.as_view(), name='python_5'),
+    path('python/python-6/', views.Python6View.as_view(), name='python_6'),
+    path('python/python-math/', views.PythonMathView.as_view(), name='python_math'),
+    path('python/python-random/', views.PythonRandomView.as_view(), name='python_random'),
+    path('html-css/html-css-index/', views.HtmlCssIndexView.as_view(), name='html_css_index'),
+    path('html-css/html-css-1/', views.HtmlCss1View.as_view(), name='html_css_1'),
+    path('search/', search.get_search, name='search'),
+    path('thinup/contact-form/', views.ContactFormView.as_view(), name='contact_form'),
+    # path('thinup/complete-form/', forms.send_email, name='form'),
 ]
